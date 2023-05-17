f0289fecac6b0a1067beb73dc72b0e1985bbfa95
Gemy98 <mohamedgamal19981998@gmail.com>
2023-05-17 03:22:25 +0300

    set Configuration and testing

---
+++ src/com/spring/dao/Student.java
@@ -0,0 +1,13 @@
+package com.spring.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+	public void myData() {
+		
+		System.out.println("Student Data");
+	}
+	
+}
